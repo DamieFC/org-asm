8571066fc88a7cc669648e0e4a4f75112a33bac1
GNUOverYooouu <dtortoise99@gmail.com>
2021-07-01 13:28:24 -0700

    Pls don't let anyone here see my screen (I'm at the library)
    Hey, "at the library" is an early green day song

--- src/org-asm.c
+++ src/org-asm.c
@@ -2,3 +2,13 @@
  * This started as a joke on Discord                          *
  * (c) 2021 gnuoveryou under CC 0 license                     *
  **************************************************************/
+
+#include <stdio.h>
+
+void org_asm(char args[], char file[]){
+  if(!file){
+    if(!args){
+      printf("Error: no arguments!");
+    }
+  }
+}
