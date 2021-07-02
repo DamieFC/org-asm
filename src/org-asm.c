343a69394d313569903450293aa63423bf2876f6
GNUOverYooouu <dtortoise99@gmail.com>
2021-07-02 10:12:39 -0700

    Hmmm

--- src/org-asm.c
+++ src/org-asm.c
@@ -4,11 +4,18 @@
  **************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 
 void org_asm(char args[], char file[]){
   if(!file){
     if(!args){
       printf("Error: no arguments!");
     }
+  }
+  else if(strcmp(args[1], "help") == 0) {
+    printf("orgasm is an assembly language and assembler that started as a joke on Discord.\n");
+    printf("Usage: \n");
+    printf("./org-asm help\n");
+    return;
   }
 }
