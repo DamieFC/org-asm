c448ac0cf70820ec6900d638ac121428d79a7b87
GNUOverYooouu <dtortoise99@gmail.com>
2021-07-15 18:52:41 -0700

    Stuff

--- src/org-asm.c
+++ src/org-asm.c
@@ -5,13 +5,15 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 void org_asm(char args[], char file[]){
   if(!file){
     if(!args){
-      printf("Error: no arguments!");
+      printf("Error: no arguments!\n");
     }
   }
+  tolower(args[]);
   else if(strcmp(args[0], "help") == 0) {
     printf("orgasm is an assembly language and assembler that started as a joke on Discord.\n");
     printf("Usage: \n");
@@ -20,11 +22,11 @@ void org_asm(char args[], char file[]){
   }
   else if(strcmp(args[0], "run") == 0) {
     if(!file) {
-      printf("Error: no files to run.");
+      printf("Error: no files to run.\n");
       return;
     }
     else if(!strcmp(file[0], ".asm") || !strcmp(file[0], ".s") >= 0) {
-      printf("Error: file not assembly (.asm or .s)!");
+      printf("Error: file not assembly (.asm or .s)! Note, has to be lowercase.\n");
       return;
     }
   }
