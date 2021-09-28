1233d38c09403be20af79f7547ca4e9e73bad391
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-28 14:11:36 -0700

    Pointer maybe

--- src/org-asm.c
+++ src/org-asm.c
@@ -37,7 +37,7 @@ int org_asm(char args[], char file[]){
         printf("Error: file not assembly (.asm or .s)! It can be uppercase and lowercase.\n");
         return(1);
       } else {
-        char code[] = fopen(file[0], "r");
+        char *code[] = fopen(file[0], "r");
         int position = 0;
         if(strcmp(code, ";") == 0) {
           while(code != "\n") {
