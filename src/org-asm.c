b44ec2b52e6b740eb295939694f55411460e2b8d
GNUOverYooouu <dtortoise99@gmail.com>
2021-10-18 13:36:29 -0700

    Update org-asm.c

--- src/org-asm.c
+++ src/org-asm.c
@@ -8,5 +8,9 @@
 #include <stddef.h>
 
 int org_asm(char usage[], char args[]) {
+  if(usage[0] = "help") {
+    printf("./orgasm <usage> <args>\n");
+    printf("Options for usage: help\n");
+  }
   printf("You're an orgasm addict!");
 }
