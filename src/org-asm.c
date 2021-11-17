30c4a34f7838a617b61f1e60e2e41a6bce777d39
GNUOverYooouu <dtortoise99@gmail.com>
2021-11-17 08:27:04 -0800

    *In crazy Mark Arm voice* I never let it bother me!
    Green River reference

--- src/org-asm.c
+++ src/org-asm.c
@@ -8,10 +8,21 @@
 #include <stddef.h>
 #include <string.h>
 
-int org_asm(char *usage[], char *args[]) {
+/*int org_asm(char *usage[], char *args[]) {
   if(strcmp(usage[0], "help") == 0) {
     printf("./orgasm <usage> <args>\n");
     printf("Options for usage: help\n");
   }
   printf("You're an orgasm addict!");
+}*/
+
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    perror("TOO MANY ARGUMENTS WAAAAAHH SO ARE WE GONNA GO 69 OR ANAL??");
+  }
+    if (strcmp(argv[0], "help") == 0) {
+      printf("./orgasm <usage> <args>\n");
+      printf("Options for usage: help\n");
+    }
+  printf("You're an orgasm addict! - The Buzzcocks");
 }
